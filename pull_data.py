1c4996abb3f8ee4b194625a846d23d8e420fddec
andrewgaldys <andrewgaldys@icloud.com>
2025-11-13 13:27:15 -0500

    SMA 50D

--- pull_data.py
+++ pull_data.py
@@ -1,4 +1,7 @@
 import yfinance as yf
 
 data = yf.download("AAPL", start="2020-01-01")
-print(data)
+data["Return"] = data["Adj Close"].pct_change()
+data["SMA_50"] = data["Adj Close"].rolling(window=50).mean()
+
+print(data.tail())
